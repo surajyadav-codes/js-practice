a0548f01d72e38ef2e2e107374728b3003f41c61
surajyadav-codes <sy0142006@gmail.com>
2026-04-26 09:36:27 +0000

    Updated variables example

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -6,6 +6,8 @@ prefer not to use var
 because of issue in block scope and functional scope
 */
 let accountState;
-accountCity ="Ludhiana"
-accountGmail = "Sky@google.com"
+accountCity ="Ludhiana";
+accountGmail = "Sky@google.com";
+accountPass = "4521";
+accountCity = "Bengaluru";
 console.table([accountGmail , accountId ,accountPass ,accountCity,accountState])
