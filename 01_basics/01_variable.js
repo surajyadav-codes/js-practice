2946ed0ae928069da57cc6e0a36b1b63371a056f
surajyadav-codes <sy0142006@gmail.com>
2026-04-26 09:29:10 +0000

    Added Variable file

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 123323;
+let accountGmail = "Suraj@google.com"
+var accountPass = "234";
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+let accountState;
+accountCity ="Ludhiana"
+accountGmail = "Sky@google.com"
+console.table([accountGmail , accountId ,accountPass ,accountCity,accountState])
