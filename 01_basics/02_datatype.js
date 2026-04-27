bbd01351b082167c2c719b745b46f249d99c4908
surajyadav-codes <sy0142006@gmail.com>
2026-04-27 11:54:31 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as a New version 
+
+// alert("Hello") // we are using node not browser 
+ 
+console.log(3
+    +3)  // code  readability 
+
+    let age = 12;
+    let name = "suraj"
+
+    // Datatypes in JS
+
+    /*
+    number => 2 to power 53
+    string => " "
+    bigint
+    boolean => true or false 
+    null => standalone value
+    undefined = 
+    symbol => unique
+
+    object 
+
+    */
+console.log(typeof "suraj");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
