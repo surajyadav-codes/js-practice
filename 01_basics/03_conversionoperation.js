b39310a9e432aa083aa514d59034e4834a7e04fa
surajyadav-codes <sy0142006@gmail.com>
2026-04-28 12:44:31 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,38 @@
+let score = "suraj"
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score))
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*
+when we convert 
+"33" => 33 
+"33hs" => NaN (Not a Number);
+null => 0 
+undefined => NaN 
+true => 1 , false => 0
+"name" => NaN
+*/
+
+let IsloggedIn = 's'
+let booleanIsLoggedIn = Boolean(IsloggedIn)
+//console.log(booleanIsLoggedIn);
+
+// "" => false 
+// "name" => true
+// 'a' => true
+
+let number = 33
+let value = String(number)
+console.log(typeof number);
+
+console.log(typeof value);
+console.log(value);
+
