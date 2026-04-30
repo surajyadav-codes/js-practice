d0b611ae2dc64013bc29e9be46c6ce02bbdfe41e
surajyadav-codes <sy0142006@gmail.com>
2026-04-30 12:39:54 +0000

    operation in js

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,8 +1,8 @@
-let score = "suraj"
+// let score = "suraj"
 // console.log(typeof score);
 // console.log(typeof (score))
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
@@ -18,18 +18,42 @@ true => 1 , false => 0
 "name" => NaN
 */
 
-let IsloggedIn = 's'
-let booleanIsLoggedIn = Boolean(IsloggedIn)
+// let IsloggedIn = 's'
+// let booleanIsLoggedIn = Boolean(IsloggedIn)
 //console.log(booleanIsLoggedIn);
 
 // "" => false 
 // "name" => true
 // 'a' => true
 
-let number = 33
-let value = String(number)
-console.log(typeof number);
+// let number = 33
+// let value = String(number)
+// console.log(typeof number);
 
-console.log(typeof value);
-console.log(value);
+// console.log(typeof value);
+// console.log(value);
+
+//  ****         Operation    ******      // 
+
+let value = 3;
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2 + 2)
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 - 2);
+
+let str1 = "hello"
+let str2 = " Suraj"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 3);
+
+
+
 
