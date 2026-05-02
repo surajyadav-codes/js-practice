7d51e83b17021fb86ff459d3b1ad8b486c5774d0
surajyadav-codes <sy0142006@gmail.com>
2026-05-02 11:17:48 +0000

    operation done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -45,15 +45,27 @@ let negvalue = -value
 // console.log(2 ** 2);
 // console.log(2 - 2);
 
-let str1 = "hello"
-let str2 = " Suraj"
+// let str1 = "hello"
+// let str2 = " Suraj"
 
-let str3 = str1 + str2
-console.log(str3);
+// let str3 = str1 + str2
+// console.log(str3);
 
-console.log(1 + "2");
-console.log("1" + 3);
+// console.log(1 + "2");
+// console.log("1" + 3);
+
+// console.log((3+4) *5 % 3);
+// console.log(+true);
+// console.log(+"");
 
 
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+ 
 
 
