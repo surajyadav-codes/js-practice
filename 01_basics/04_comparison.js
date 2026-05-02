48d0868fba31b8ad73b112e9f2d7e18412d358a9
surajyadav-codes <sy0142006@gmail.com>
2026-05-02 11:52:22 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 5);
+// console.log(2 >= 1);
+// console.log( 2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);      // false 
+// console.log(null == 0);     // false
+// console.log(null < 0);     // false 
+
+// console.log(null >= 0);    // true 
+//  console.log(null <= 0);    // true
+// /*
+// The reason is that an eqaulity check == and comparison >, < , <= , >= work differently 
+// comparison converts the null to a number , treating it as 0. 
+// that's why null  >= 0. is true and null > 0 is false
+// */
+// console.log(undefined == 0);  // false
+// console.log(undefined >= 0);  // false
+// console.log(undefined > 0);   // false
+
+
+// === 
+console.log("2" == 2); // true
+console.log("2" === 2);  // false 
+
+
